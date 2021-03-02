df4242042e20cad42273a48b9c41dbfa9f944704
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-03-02 17:14:17 +0900

    Update run.py

--- run.py
+++ run.py
@@ -9,6 +9,18 @@ while True:
     t = '"' + "temperature[degree]" + '"' + ":" + '"' + str(round(sense.get_temperature(), 1)) + '"'
     p = '"' + "pressure[hPa]" + '"' + ":" + '"' + str(round(sense.get_pressure(), 1)) + '"'
     h = '"' + "humidity[%]" + '"' + ":" + '"' + str(round(sense.get_humidity(), 1)) + '"'
-    mylist = [t,p,h]
+    
+    orientation = sense.get_orientation()
+    pitch = '"' + "pitch" + '"' + ":" + '"' + str(round(orientation["pitch"], 0)) + '"'
+    roll = '"' + "roll" + '"' + ":" + '"' + str(round(orientation["roll"], 0)) + '"'
+    yaw = '"' + "roll" + '"' + ":" + '"' + str(round(orientation["yaw"], 0)) + '"'
+    
+    
+    acceleration = sense.get_accelerometer_raw()
+    x = '"' + "x" + '"' + ":" + '"' + round(str(acceleration['x']),0) + '"'
+    y = '"' + "y" + '"' + ":" + '"' + round(str(acceleration['y']),0) + '"'
+    z = '"' + "z" + '"' + ":" + '"' + round(str(acceleration['z']),0) + '"'
+    
+    mylist = [t,p,h,pitch,roll,yaw,x,y,z]
     mystr = '{' + ','.join(map(str,mylist))+'}'
     print(mystr)
