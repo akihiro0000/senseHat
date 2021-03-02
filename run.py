9f66ffe279fd6d66639ea15919ac21c8dfae902a
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2021-03-02 16:46:57 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,17 +1,15 @@
 from sense_hat import SenseHat
+import time
+
 sense = SenseHat()
 while True:
   t0 = time.time()
   
   if ((time.time() - t0) > 60):
-    # 温度と湿度、気圧を測定
     t = sense.get_temperature()
     p = sense.get_pressure()
     h = sense.get_humidity()
-    # 小数点以下第1位に四捨五入
     t = round(t, 1)
     p = round(p, 1)
     h = round(h, 1)
-    # 文字表示内容の設定
-
     print(t)
